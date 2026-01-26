4949f61b105c8d609e3615d07f4b2fa26b192e39
devSeyeong <126233883+devSeyeong@users.noreply.github.com>
2026-01-26 18:20:57 +0900

    Update stack_queuestack.java

--- stack_queuestack.java
+++ stack_queuestack.java
@@ -30,11 +30,11 @@ public class Main {
         // 큐보다 덱을 쓰는것이 효율이 좋다.
         Deque<Integer> queue = new ArrayDeque<>();
 
-        // 스택이면 절대 나올일이 없어서 필요 없다. 큐에 들어있는 것들이 순서대로 나올 것.
+        // 스택이면 절대 나올일이 없어서 필요 없다. 큐에 들어있는 것들이 뒤부터 순서대로 나올 것.
         for (int i = 0 ;i < N; i++){
             int val = Integer.parseInt(st.nextToken());
             if (!isStack[i]) {
-                queue.offerLast(val);
+                queue.offerFirst(val);
             }
         }
 
@@ -44,6 +44,7 @@ public class Main {
 
         for (int i = 0 ;i < count; i++){
             int input = Integer.parseInt(st.nextToken());
+            queue.offerLast(input);
 
             if (queue.isEmpty()) {
                 // 큐가 없으면 그대로 출력
@@ -52,8 +53,9 @@ public class Main {
                 // 큐가 있으면 정상 동작
                 int result = queue.pollFirst();
                 bw.write(result + " ");
-                queue.offerLast(input);
             }
+
+
         }
 
         bw.flush();
