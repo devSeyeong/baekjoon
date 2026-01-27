61d2791bd2a2e2f9dd6e7da9481992ceef3e94d8
devSeyeong <126233883+devSeyeong@users.noreply.github.com>
2026-01-27 13:45:02 +0900

    Create 동적계획법_신나는함수실행.java

---
+++ 동적계획법_신나는함수실행.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+import java.io.IOException;
+import java.lang.Math;
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int N = Integer.parseInt(st.nextToken());
+
+
+        // dp 초기화
+        for (int i = 0; i <= 20; i++) {
+            for (int j = 0; j <= 20; j++) {
+                for (int k = 0; k <= 20; k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+
+        while (true) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a == -1 && b == -1 && c == -1) break;
+
+            int result = w(a, b, c);
+            bw.write("w("+a+", "+b+", "+c+") = "+ result+"\n");
+        }
+        bw.flush();
+        bw.close();
+
+    }
+
+    static int[][][] dp = new int[21][21][21];
+
+    static int w(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) return 1;
+        if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
+
+        if (dp[a][b][c] != -1) return dp[a][b][c]; // 이미 계산했으면 반환
+
+        if (a < b && b < c) {
+            dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+        } else {
+            dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        }
+
+        return dp[a][b][c];
+    }
+
+}
